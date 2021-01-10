95cbce0db681bd8b97ea88e66c8ae1d9e4e63785
Bardia323 <Bardia.shahrestani@gmail.com>
2021-01-10 18:59:43 -0500

    Added try and made files modular

---
+++ forms.py
@@ -0,0 +1,22 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField, BooleanField
+from wtforms.validators import DataRequired, Length, EqualTo
+import email_validator
+
+class RegistrationForm(FlaskForm):
+    username = StringField('Username',
+                           validators=[DataRequired(), Length(min=4, max=20)])
+    email = StringField('Email',
+                        validators=[DataRequired(), Email()])
+    password = PasswordField('Password', validators=[DataRequired(), Length(min=6,max=22), ])
+    confirm_password = PasswordField('Confirm Password',
+                                     validators=[DataRequired(), EqualTo('password')])
+    submit = SubmitField('Sign Up')
+
+
+class LoginForm(FlaskForm):
+    email = StringField('Email',
+                        validators=[DataRequired(), Email()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    remember = BooleanField('Remember Me')
+    submit = SubmitField('Login')
--- main.py
+++ main.py
@@ -8,8 +8,10 @@ from flask import request
 from flask import url_for
 from datetime import datetime
 from random import randint
-
+#from forms import *
 import json
+import requests
+from werkzeug.datastructures import Headers
 
 
 def import_list_from_txt(list_from_txt):
@@ -26,15 +28,57 @@ ads = import_list_from_txt(os.getcwd()+'/500 UBIK for UBIK ads.txt').split('\n\n
 
 # Create a Flask instance
 app = Flask(__name__, template_folder='template')
+app.config['SECRET_KEY'] = '3cbf224240f0b822365c90a5f6ebe108b5'
 
 # Return a template
 @app.route('/')
 def index():
-    return render_template('index.html', Tagline=taglines[randint(0,len(taglines))].split("Slogan:", 1)[1], Paragraph= ads[randint(0,len(ads))])
+    return render_template('index.html', 
+                           Tagline=taglines[randint(0,len(taglines))]
+                           .split("Slogan:", 1)[1], 
+                           Paragraph= ads[randint(0,len(ads))])
 
+@app.route('/try')
+def tryy():
+    return render_template('try.html')
+    
+    
+    
+""" @app.route('/payment')
+def regiter():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        flash(f'Account created for {form.username.data}!', 'success')
+        return redirect(url_for('home'))
+    return render_template('register.html', title='Register', form=form) """
+
+
+""" @app.route('/Login')
+def login():
+    def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        if form.email.data == 'admin@blog.com' and form.password.data == 'password':
+            flash('You have been logged in!', 'success')
+            return redirect(url_for('home'))
+        else:
+            flash('Login Unsuccessful. Please check username and password', 'danger')
+    return render_template('login.html', title='Login', form=form) """
+
+@app.route('/Minds/Slogans')
+def slogans():
+    pass
+    
+@app.route('/Minds/Reviews')
+def reviews():
+    pass
+@app.route('/Minds/Ads')
+def Ads():
+    pass
+    
 # Run the app
 def main():
-    app.run()
+    app.run(debug=True)
 
 
 if __name__ == "__main__":
